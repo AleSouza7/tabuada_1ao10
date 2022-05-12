2e13c09a21f91b633f48790403ba7942e17f12bb
Alexsouza <96217395+AleSouza7@users.noreply.github.com>
2022-05-12 19:52:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+for(var y = 1; y <=10; y++) {
+
+document.write('<h3> Tabuada do <h3/>'+y)
+document.write('<br/>'+'<br/>')
+
+for(var x = 1; x <=10; x++) {
+	document.write(y + ' X ' + x +' = '+(y*x)+'<br/>')
+}
+
+document.write('<br/><hr/><br/>')
+
+}
